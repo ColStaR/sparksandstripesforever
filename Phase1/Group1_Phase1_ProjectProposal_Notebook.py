d3bd579ee0738238ca470282286ba3deca8f13a5
starshootr@gmail.com <starshootr@gmail.com>
2022-10-28 05:13:17 +0000

    Added Phase Folders, Added Phase 1 Project Proposal notebook

---
+++ Phase1/Group1_Phase1_ProjectProposal_Notebook.py
@@ -0,0 +1,120 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Team 13 Phase 1 Project Proposal
+# MAGIC ## Section 4, Group 1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC The deliverable should be a notebook. Will need to copy Google Doc content over here prior to submission.
+# MAGIC 
+# MAGIC Google Doc Link:
+# MAGIC https://docs.google.com/document/d/1ZCUOfiGdChziaCCqxihUFIBQIjL8mRaQNTz-vA0Fhk0/edit#
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Project Title
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Phase Leader Plan
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Credit Assignment Plan
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Project Abstract
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Introduction
+# MAGIC 
+# MAGIC Discretionary if we have time. Dump otherwise
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## The Data
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## The Desired Outcome
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## The Data Pipeline
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Data Ingesting
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Joining Datasets
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Machine Learning Algorithms to be Used
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Resource Management and Performance Optimization
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Train/Test/Validation Data Splits
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## GANTT Chart for Project Timeline and Tasks
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Conclusions and Next Steps
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Open Issues or Problems
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Team Names and Photos
+
+# COMMAND ----------
+
+
