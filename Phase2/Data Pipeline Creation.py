5de2c6f0b47aaa418d6e3e668a2634175a914f3d
starshootr@gmail.com <starshootr@gmail.com>
2022-11-06 18:50:39 +0000

    Created Data Pipeline Creation notebook

---
+++ Phase2/Data Pipeline Creation.py
@@ -0,0 +1,65 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # Data Pipeline Creation
+# MAGIC 
+# MAGIC Useful links? 
+# MAGIC 
+# MAGIC https://towardsdatascience.com/building-an-ml-application-with-mllib-in-pyspark-part-1-ac13f01606e2
+# MAGIC 
+# MAGIC https://docs.databricks.com/machine-learning/train-model/mllib/index.html
+# MAGIC 
+# MAGIC https://docs.databricks.com/_static/notebooks/binary-classification.html
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, floor
+from pyspark.sql.functions import isnan, when, count, col
+from pyspark.sql.types import IntegerType
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+import pandas as pd
+
+# COMMAND ----------
+
+# Azure Storage Container info
+from pyspark.sql.functions import col, max
+
+blob_container = "sasfcontainer" # The name of your container created in https://portal.azure.com
+storage_account = "sasfstorage" # The name of your Storage account created in https://portal.azure.com
+secret_scope = "sasfscope" # The name of the scope created in your local computer using the Databricks CLI
+secret_key = "sasfkey" # The name of the secret key created in your local computer using the Databricks CLI 
+blob_url = f"wasbs://{blob_container}@{storage_account}.blob.core.windows.net"
+mount_path = "/mnt/mids-w261"
+
+# COMMAND ----------
+
+# SAS Token login
+spark.conf.set(
+  f"fs.azure.sas.{blob_container}.{storage_account}.blob.core.windows.net",
+  dbutils.secrets.get(scope = secret_scope, key = secret_key)
+)
+
+# COMMAND ----------
+
+# Load Dataframes
+print("**Loading Data")
+
+# Inspect the Joined Data folders 
+display(dbutils.fs.ls(f"{blob_url}"))
+
+print("**Data Loaded")
+print("**Loading Data Frames")
+
+df_joined_data_3m = spark.read.parquet(f"{blob_url}/joined_data_3m")
+display(df_joined_data_3m)
+
+df_joined_data_3m = spark.read.parquet(f"{blob_url}/joined_data_all")
+display(df_joined_data_3m)
+
+print("**Data Frames Loaded")
+
+# COMMAND ----------
+
+
