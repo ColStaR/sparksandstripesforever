b643ebf722e453dc24bbe76c3bc33b5f8f85f304
Cabralnc96 <Cabralnc96>
2022-11-28 04:53:15 +0000

    NASHUPDATE

--- Phase3/PageRank Feature Join With eFeatures.py
+++ Phase3/PageRank Feature Join With eFeatures.py
@@ -74,9 +74,57 @@ df_join_PR.count()
 
 # COMMAND ----------
 
-df_join_PR.write.mode("overwrite").parquet(f"{blob_url}/joined_all_with_efeatures_v2_No2015")
+#df_join_PR.write.mode("overwrite").parquet(f"{blob_url}/joined_all_with_efeatures_v2_No2015")
 
 
 # COMMAND ----------
 
+df_join_PR = df_join_PR.drop('DEP_DATETIME_LAG', 'UTC_DEP_DATETIME_LAG', 'UTC_DEP_DATETIME')
+
+# COMMAND ----------
+
+df2 = df_join_PR.select([count(when(col(c).contains('None') | \
+                            col(c).contains('NULL') | \
+                            (col(c) == '' ) | \
+                            col(c).isNull() | \
+                            isnan(c), c 
+                           )).alias(c)
+                    for c in df_join_PR.columns])
+df2.show()
+
+
+# COMMAND ----------
+
+PageranksByYear = PageranksByYear.withColumn("pagerank",PageranksByYear.pagerank.cast('double'))
+display(PageranksByYear)
+
+#display(PageranksByYear.filter(PageranksByYear.YEAR2 == "2015"))
+#display(PageranksByYear.filter(PageranksByYear.YEAR2 == "2016"))
+
+
+
+# COMMAND ----------
+
+# Azure Storage Container info
+from pyspark.sql.functions import col, max
+
+blob_container = "sasfcontainer" # The name of your container created in https://portal.azure.com
+storage_account = "sasfstorage" # The name of your Storage account created in https://portal.azure.com
+secret_scope = "sasfscope" # The name of the scope created in your local computer using the Databricks CLI
+secret_key = "sasfkey" # The name of the secret key created in your local computer using the Databricks CLI 
+blob_url = f"wasbs://{blob_container}@{storage_account}.blob.core.windows.net"
+mount_path = "/mnt/mids-w261"
+
+# SAS Token login
+spark.conf.set(
+  f"fs.azure.sas.{blob_container}.{storage_account}.blob.core.windows.net",
+  dbutils.secrets.get(scope = secret_scope, key = secret_key)
+)
+
+PageranksByYear = spark.read.csv(f"{blob_url}/PageranksByYear.csv", header=True)
+PageranksByYear = PageranksByYear.withColumnRenamed('YEAR','YEAR2').distinct()
+
+
+PageranksByYear = PageranksByYear.withColumn("pagerank",PageranksByYear.pagerank.cast('double'))
+display(PageranksByYear)
 
